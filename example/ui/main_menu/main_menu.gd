f702a0303a47116462516d1c520f5502465ecab1
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2026-03-30 06:42:00 +0100

    Add options menu to main menu

--- example/ui/main_menu/main_menu.gd
+++ example/ui/main_menu/main_menu.gd
@@ -1,5 +1,10 @@
 extends Control
+## Example game main menu
+##
+## Main menu for the example game
 
+@onready var _main_menu_panel: Control = $MarginContainer/MainMenuPanel
+@onready var _options_menu: Control = $OptionsMenu
 
 # Signal connection
 func _on_play_button_pressed() -> void:
@@ -8,9 +13,16 @@ func _on_play_button_pressed() -> void:
 
 # Signal connection
 func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
+	_main_menu_panel.visible = false
+	_options_menu.visible = true
 
 
 # Signal connection
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+# Signal Connection
+func _on_options_menu_back_pressed() -> void:
+	_main_menu_panel.visible = true
+	_options_menu.visible = false
---
+++ example/ui/options_menu/options_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+## Example game options menu
+##
+## User interface containing all options for the example game
+
+## Emitted when the back button is predd
+signal back_pressed
+
+
+# Signal connection
+func _on_back_button_pressed() -> void:
+	back_pressed.emit()
